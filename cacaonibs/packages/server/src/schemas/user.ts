0fa035d859f637d2b1f4d75a760d2d7c4fad82cd
kimteasick <notseph8556@gmail.com>
2022-09-13 15:41:50 +0900

    making cacaonibs

---
+++ cacaonibs/packages/server/src/schemas/user.ts
@@ -0,0 +1,17 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../sequelize";
+
+const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: DataTypes.STRING,
+  thumbnailImageUrl: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+export default User;
